ccee44a7642fe4c95123d94375bfea11985d07a3
Anthony Manzon <114978074+markanthonymanzon@users.noreply.github.com>
2022-10-15 02:05:36 +0800

    Add files via upload

---
+++ DAY16_HTML/DAY16/Register.php
@@ -0,0 +1,14 @@
+
+  <html>
+     <body >
+      NAME: <?php echo $_POST["NAME"]; ?> <br>
+      COURSE: <?php echo $_POST["COURSE"]; ?> <br>
+      ADDRESS : <?php echo $_POST["ADDRESS"]; ?> <br>
+      CONTACT : <?php echo $_POST["CONTACT"]; ?> <br>
+   </body>
+</html> 
+
+
+
+
+
